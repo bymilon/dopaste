093b9f363a44cedc29913c5b19641f24fd048900
pitechae <pitechae@gmail.com>
2026-05-30 06:52:15 +0400

    Delete table.sql

--- migrations/0000_create_pastes_table.sql
+++
@@ -1,8 +0,0 @@
--- Migration: Create pastes table
-CREATE TABLE IF NOT EXISTS pastes (
-  id TEXT PRIMARY KEY,
-  title TEXT NOT NULL,
-  content TEXT NOT NULL,
-  language TEXT NOT NULL DEFAULT 'plaintext',
-  created_at INTEGER NOT NULL
-);
